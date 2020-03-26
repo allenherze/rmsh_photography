57ba3242a3986bee00d247038c75d2c89a9983b9
allenherze <joshuac6378@gmail.com>
2020-03-26 19:23:10 +0530

    Netlify Deploy

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// For Mobile navigation to fade out on scroll
+const toggle = document.getElementById("toggler");
+
+window.addEventListener("scroll", () => (toggle.checked = false));
+
+const buttons = document.querySelector(".category").children;
+const items = document.querySelector(".image-gallery").children;
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function() {
+    const target = this.getAttribute("data-target");
+    for (let j = 0; j < items.length; j++) {
+      items[j].style.display = "none";
+
+      if (items[j].getAttribute("data-id") === target) {
+        items[j].style.display = "block";
+      }
+
+      if (target === "all") {
+        items[j].style.display = "block";
+      }
+    }
+  });
+}
