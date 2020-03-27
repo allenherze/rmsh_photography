fab6428c82a4947cd30cfae64dd2ce2143cfb4e0
allenherze <joshuac6378@gmail.com>
2020-03-27 09:39:15 +0530

    Major update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 // For Mobile navigation to fade out on scroll
 const toggle = document.getElementById("toggler");
-
 window.addEventListener("scroll", () => (toggle.checked = false));
 
 const buttons = document.querySelector(".category").children;
