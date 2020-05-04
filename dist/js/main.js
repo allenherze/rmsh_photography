520f0b64a8fdaff08d7c0935cd002c4bacf37ff1
allenherze <joshuac6378@gmail.com>
2020-05-04 23:12:37 +0530

    Added JS

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,28 @@
-const overlay = document.querySelector('.overlay');
+const mainNav = document.getElementById('js-menu');
+const navBar = document.querySelector('#navbar');
+const navBtn = document.getElementById('js-navbar-toggle');
+let deskMatch = window.matchMedia('min-width:801px');
+let scrollPosition = window.scrollY;
 
-setTimeout(() => {
-  overlay.style.display = 'none';
-}, 1000);
+navBtn.addEventListener('click', openMenu);
+window.addEventListener('scroll', closeMenu);
+deskMatch.addEventListener('scroll', navDark);
+
+function openMenu() {
+  mainNav.style.visibility = 'visible';
+  mainNav.style.opacity = 1;
+}
+
+function closeMenu() {
+  mainNav.style.visibility = 'hidden';
+  mainNav.style.opacity = 0;
+}
+
+function navDark() {
+  scrollPosition = window.scrollY;
+  if (scrollPosition >= 30) {
+    navBar.classList.add('bg-dark');
+  } else {
+    navBar.classList.remove('bg-dark');
+  }
+}
