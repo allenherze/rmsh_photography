81e32482e281f2eb096dd578921b4b0a62e62bae
allenherze <joshuac6378@gmail.com>
2020-05-06 22:48:23 +0530

    Added CSS

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,28 +1,12 @@
 const mainNav = document.getElementById('js-menu');
 const navBar = document.querySelector('#navbar');
 const navBtn = document.getElementById('js-navbar-toggle');
-let deskMatch = window.matchMedia('min-width:801px');
-let scrollPosition = window.scrollY;
+const closeBtn = document.querySelector('.close');
 
-navBtn.addEventListener('click', openMenu);
-window.addEventListener('scroll', closeMenu);
-deskMatch.addEventListener('scroll', navDark);
-
-function openMenu() {
+navBtn.addEventListener('click', function openMenu() {
   mainNav.style.visibility = 'visible';
-  mainNav.style.opacity = 1;
-}
+});
 
-function closeMenu() {
+closeBtn.addEventListener('click', function closeMenu() {
   mainNav.style.visibility = 'hidden';
-  mainNav.style.opacity = 0;
-}
-
-function navDark() {
-  scrollPosition = window.scrollY;
-  if (scrollPosition >= 30) {
-    navBar.classList.add('bg-dark');
-  } else {
-    navBar.classList.remove('bg-dark');
-  }
-}
+});
